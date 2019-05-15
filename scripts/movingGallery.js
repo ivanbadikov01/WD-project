a4711d42f49ea21f768cd3ef0fba84a870796efe
ivanbadikov01 <43810114+ivanbadikov01@users.noreply.github.com>
2019-05-15 20:55:08 +0300

    (almost) fininished

--- scripts/movingGallery.js
+++ scripts/movingGallery.js
@@ -1,34 +1,54 @@
 $(document).ready(function() {
     var gallery = $('#movingGallery');
-    var images = ["../pictures/cat1.jpg", "../pictures/cat2.jpg", "../pictures/cat3.jpg"];
+    var slide1 = { img: "../pictures/cat1.jpg", heading: "Meet Kitty", innerText: "Kitty is the cuddliest kitten in Varna.", redirect: "../pages/kitty.html" };
+    var slide2 = { img: "../pictures/cat2.jpg", heading: "Meet Neko", innerText: "Neko is a favorite among the locals in Tokyo, Japan.", redirect: "../pages/neko.html" };
+    var slide3 = { img: "../pictures/cat3.jpg", heading: "Meet Garry", innerText: "Garry is the cuddliest kitten in Plovdiv.", redirect: "../pages/garry.html" };
+    var slides = [slide1, slide2, slide3];
+    var heading = $("#galleryHeading");
+    var gText = $("#galleryText");
+    var bttn = $(".redirect");
     var i = 1;
 
     $("#left").click(function(event) {
-        console.log(i);
         if (i > 0) {
             i--;
 
-            var img = "url('" + images[i] + "')";
+            var slide = slides[i];
+            var img = "url('" + slide.img + "')";
+
             $(gallery).fadeOut('fast', function() {
-            
                 $(gallery).css("background-image", img);
-            	$(gallery).fadeIn('fast');
+                $(heading).text(slide.heading);
+                $(gText).text(slide.innerText);
+                $(bttn).attr('href', slide.redirect);
+                $(gallery).fadeIn('fast');
             });
 
         }
     });
 
     $("#right").click(function(event) {
-        console.log(i);
         if (i < 2) {
             i++;
-            $(gallery).fadeOut('fast');
-            var img = "url('" + images[i] + "')";
-            console.log(img);
-            $('#movingGallery').css("background-image", img);
-            $('#movingGallery').fadeIn('fast');
+
+            var slide = slides[i];
+            var img = "url('" + slide.img + "')";
+
+            $(gallery).fadeOut('fast', function() {
+                $(gallery).css("background-image", img);
+                $(heading).text(slide.heading);
+                $(gText).text(slide.innerText);
+                $(bttn).attr('href', slide.redirect);
+                $(gallery).fadeIn('fast');
+            });
         }
     });
 
-
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 0) {
+            $(gallery).slideUp('slow');
+        } else {
+            $(gallery).slideDown('slow');
+        }
+    });
 });
---
+++ scripts/parallax.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    var position = $(window).scrollTop();
+    var dimmer = $("#dimmer");
+    var opac = 0.2;
+
+
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll > position) {
+            if (opac < 1) {
+                opac += 0.01;
+            }
+            $("#dimmer").fadeTo(5, opac);
+
+        } else {
+            if (opac > 0 && $(this).scrollTop() < 746) {
+                opac -= 0.01;
+                $("#dimmer").fadeTo(5, opac);
+                
+            }
+        }
+
+        if ($(window).scrollTop() == 0) {
+            opac = 0.2;
+            $(dimmer).fadeTo(300, opac);
+            
+        }
+
+        position = scroll;
+    });
+
+
+});
